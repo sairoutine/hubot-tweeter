26613200c1c9eb9ecf59ab9b7932cee53afd5180
hubot <hubot@hubot.cs666idcfcloud.internal>
2015-07-10 14:33:18 +0900

    add cron_message.json

--- scripts/cron.js
+++ scripts/cron.js
@@ -1,16 +1,40 @@
 var myname  = "@sairoutine";
 var cronJob = require('cron').CronJob;
 
+/* 定期的につぶやく内容一覧 */
+var fs = require('fs');
+var path = require('path').resolve(__dirname, '../cron_message.json');
+
+var cron_messages = JSON.parse(fs.readFileSync(path, 'utf-8'));
+/* ランダムなタイミングでつぶやく内容一覧 */
+//var random_messages = require('../random_messages.json');
+
 module.exports = function(robot) {
+	cron_messages.forEach(function(mc){
+		new cronJob({
+			cronTime: mc.time,
+
+			onTick: function() {
+				robot.send({room: "#general"}, myname + " " + mc.message);
+			},
+
+			start: true,
+			timeZone: "Asia/Tokyo"
+		});
+	});
+
+	/* ランダムなタイミングでつぶやく */
 	new cronJob({
-		//平日13時
-		cronTime: "0 0 13 * * 1-5",
+		cronTime: "0 30 10-18 * * 1-5",
 
 		onTick: function() {
-    		robot.send({room: "#general"}, myname + " " + "昼飯の時間だぜ");
+			if (Math.random() < 0.3){
+				robot.send({room: "#general"}, myname + " " + "進捗どうだ？");
+			}
 		},
 
 		start: true,
 		timeZone: "Asia/Tokyo"
 	});
+
 };
